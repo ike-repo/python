42e67013ab390a7cfa0d9a05cc6ae059e907e6ce
ike <noname@gmail.com>
2020-07-12 13:00:36 -0400

    fibonacci assignmant update to list name

--- assign_5_fibonacci.py
+++ assign_5_fibonacci.py
@@ -1,6 +1,6 @@
 # fibonacci numbers is a sequence that 
 # each number is the sum of the two preceding ones, starting from 0 and 1
-l = list(range(11))
+fibo = list(range(11))
 for i in range(2,11):
-    l[i] = l[i-1]+l[i-2]
-print(l)
+    fibo[i] = fibo[i-1]+fibo[i-2]
+print(fibo)
